2bf726dd64327a54e771b44e8cd9d4a43f8076ff
EMachad0 <elitonmachadod200@gmail.com>
2025-05-30 19:22:36 -0300

    feat: add interrupts tests

--- tests/test_roms/gb_test_roms/cpu_instrs.rs
+++ tests/test_roms/gb_test_roms/cpu_instrs.rs
@@ -56,3 +56,31 @@ fn test_blargg_cpu_instrs_01_special() {
         String::from_utf8_lossy(output_buffer)
     );
 }
+
+#[test]
+fn test_blargg_cpu_instrs_02_interrupts() {
+    yagber::init_tracing();
+
+    let test_name = "02-interrupts.gb";
+    let rom_path = PathBuf::from(INDV_ROM_PATH).join(test_name);
+    assert!(rom_path.exists(), "Test ROM {rom_path:?} not found!");
+
+    let out_log_path = PathBuf::from("out")
+        .join(INDV_ROM_PATH)
+        .join(format!("{test_name}.log"));
+
+    let rom = fs::read(rom_path).expect("Failed to read ROM");
+    let mut emu = Emulator::new()
+        .with_cartridge(&rom)
+        .with_serial_output_file(out_log_path.to_str().unwrap())
+        .with_serial_output_buffer();
+
+    let status = run_emulator(&mut emu);
+
+    let output_buffer = emu.get_serial_output_buffer().unwrap();
+    assert!(
+        status.is_success(),
+        "Output buffer:\n{}",
+        String::from_utf8_lossy(output_buffer)
+    );
+}
