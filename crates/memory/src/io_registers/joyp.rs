b00a29f37d068220ff03283771a383ddb2547a6c
EMachad0 <elitonmachadod200@gmail.com>
2025-07-15 10:59:46 -0300

    Fix: joyp patches

--- crates/memory/src/io_registers/joyp.rs
+++ crates/memory/src/io_registers/joyp.rs
@@ -3,7 +3,7 @@ pub struct JoypRegister;
 impl JoypRegister {
     pub fn joyp_transformer(_old_value: u8, new_value: u8) -> Option<u8> {
         let selected_bits = new_value & 0x30;
-        Some(0xFF | selected_bits | 0xC0)
+        Some(0xC0 | selected_bits | 0x0F)
     }
 }
 
